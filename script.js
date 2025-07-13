60bfcd56fd4162bd5113aa309ad9d415dd1bd85c
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2025-07-13 20:07:24 +0530

    Introduction To Javascript
    Some basic concepts and assignments on JavaScript.

---
+++ script.js
@@ -0,0 +1,35 @@
+// let x = "Anuj";
+// x = "Shivam";
+
+// for (let i = 0; i < 10; i++) {}
+
+// console.log(x);
+
+// const obj = {
+//     name: "Anuj",
+//     age: 28,
+// };
+
+// console.log(obj.name);
+
+const x = -7;
+
+const y = "-7";
+
+console.log(x === y);
+
+const z = x == -7 ? "hello" : "bye";
+
+console.log(z);
+
+if ("0") {
+    console.log("it is true");
+} else {
+    console.log("it is false");
+}
+
+// const y = Number(x);
+
+// console.log(y);
+
+// console.log(typeof y);
